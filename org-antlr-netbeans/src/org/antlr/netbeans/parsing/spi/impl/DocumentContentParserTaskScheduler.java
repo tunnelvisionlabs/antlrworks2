653ff7f6a54a0b0ed2065e406579150ad1aaa8c9
sharwell <devnull@localhost>
2011-12-19 20:07:11 +0000

    AW2/NB: Implement DocumentContextParserTaskScheduler

--- org-antlr-netbeans/src/org/antlr/netbeans/parsing/spi/impl/DocumentContentParserTaskScheduler.java
+++ org-antlr-netbeans/src/org/antlr/netbeans/parsing/spi/impl/DocumentContentParserTaskScheduler.java
@@ -27,18 +27,102 @@
  */
 package org.antlr.netbeans.parsing.spi.impl;
 
-import org.antlr.netbeans.parsing.spi.ParserTaskManager;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Document;
+import javax.swing.text.JTextComponent;
+import org.antlr.netbeans.editor.text.VersionedDocumentUtilities;
 import org.antlr.netbeans.parsing.spi.ParserTaskScheduler;
+import org.netbeans.api.editor.EditorRegistry;
+import org.openide.util.lookup.ServiceProvider;
 
 /**
  * A task scheduler which schedules tasks when the content of a document changes.
  *
  * @author Sam Harwell
  */
+@ServiceProvider(service=ParserTaskScheduler.class)
 public class DocumentContentParserTaskScheduler extends ParserTaskScheduler {
+    private final Map<Document, Set<JTextComponent>> documents = new HashMap<Document, Set<JTextComponent>>();
+    private final EditorRegistryListener editorRegistryListener = new EditorRegistryListener();
+    private final DocumentListener documentListener = new DocumentListenerImpl();
 
     @Override
     protected void initializeImpl() {
+        EditorRegistry.addPropertyChangeListener(editorRegistryListener);
+    }
+
+    private class EditorRegistryListener implements PropertyChangeListener {
+
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            if (evt.getPropertyName() == null
+                || evt.getPropertyName().equals(EditorRegistry.FOCUSED_DOCUMENT_PROPERTY)
+                || evt.getPropertyName().equals(EditorRegistry.FOCUS_GAINED_PROPERTY)) {
+
+                JTextComponent editor = EditorRegistry.focusedComponent();
+                if (editor == null) {
+                    return;
+                }
+
+                Document document = editor.getDocument();
+
+                Set<JTextComponent> components;
+                synchronized (documents) {
+                    components = documents.get(document);
+                    if (components == null) {
+                        components = new HashSet<JTextComponent>();
+                        documents.put(document, components);
+                        document.addDocumentListener(documentListener);
+                        schedule(VersionedDocumentUtilities.getVersionedDocument(document));
+                    }
+                }
+
+                components.add(editor);
+            } else if (evt.getPropertyName().equals(EditorRegistry.COMPONENT_REMOVED_PROPERTY)) {
+                JTextComponent editor = (JTextComponent)evt.getOldValue();
+                if (editor == null) {
+                    return;
+                }
+
+                Document document = editor.getDocument();
+                synchronized (documents) {
+                    Set<JTextComponent> components = documents.get(document);
+                    if (components != null && components.remove(editor)) {
+                        if (components.isEmpty()) {
+                            document.removeDocumentListener(documentListener);
+                            documents.remove(document);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    private class DocumentListenerImpl implements DocumentListener {
+
+        @Override
+        public void insertUpdate(DocumentEvent e) {
+            schedule(VersionedDocumentUtilities.getVersionedDocument(e.getDocument()));
+        }
+
+        @Override
+        public void removeUpdate(DocumentEvent e) {
+            schedule(VersionedDocumentUtilities.getVersionedDocument(e.getDocument()));
+        }
+
+        @Override
+        public void changedUpdate(DocumentEvent e) {
+            schedule(VersionedDocumentUtilities.getVersionedDocument(e.getDocument()));
+        }
+
     }
 
 }
