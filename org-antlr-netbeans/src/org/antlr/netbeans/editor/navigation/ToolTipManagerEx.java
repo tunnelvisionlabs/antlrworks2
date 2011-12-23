a7ddfb112ece1f5e23d316eb707fcd7e66453746
sharwell <devnull@localhost>
2011-12-23 02:38:07 +0000

    AW2/NB: Organize imports

--- org-antlr-netbeans/src/org/antlr/netbeans/editor/navigation/ToolTipManagerEx.java
+++ org-antlr-netbeans/src/org/antlr/netbeans/editor/navigation/ToolTipManagerEx.java
@@ -44,8 +44,25 @@
 
 package org.antlr.netbeans.editor.navigation;
 
-import java.awt.event.*;
-import java.awt.*;
+import java.awt.AWTEvent;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.Window;
+import java.awt.event.AWTEventListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseMotionListener;
 import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -71,7 +88,7 @@ import org.openide.util.RequestProcessor;
 import org.openide.util.Utilities;
 
 /**
- * Customized copy of javax.swing.ToolTipManager
+ * Customized copy of {@link javax.swing.ToolTipManager}
  * 
  * @author S. Aubrecht
  */
