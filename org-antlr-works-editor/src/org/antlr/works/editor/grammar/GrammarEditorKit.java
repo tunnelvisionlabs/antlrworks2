b7c872eed9d066bd07e95b4de96e4c334779ed7f
Sam Harwell <sam@tunnelvisionlabs.com>
2012-07-20 15:41:58 -0500

    Add and implement the Go to Declaration context menu command for grammars

--- org-antlr-works-editor/src/org/antlr/works/editor/grammar/GrammarEditorKit.java
+++ org-antlr-works-editor/src/org/antlr/works/editor/grammar/GrammarEditorKit.java
@@ -17,10 +17,15 @@ import java.util.prefs.Preferences;
 import javax.swing.Action;
 import javax.swing.Icon;
 import javax.swing.JEditorPane;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 import javax.swing.JToggleButton;
+import javax.swing.KeyStroke;
 import javax.swing.text.Document;
 import javax.swing.text.EditorKit;
 import javax.swing.text.JTextComponent;
+import javax.swing.text.Keymap;
+import javax.swing.text.StyledDocument;
 import javax.swing.text.TextAction;
 import org.antlr.netbeans.editor.commenting.BlockCommentFormat;
 import org.antlr.netbeans.editor.commenting.ExtendedCommentAction;
@@ -31,13 +36,18 @@ import org.antlr.netbeans.editor.text.DocumentSnapshot;
 import org.antlr.netbeans.editor.text.VersionedDocumentUtilities;
 import org.antlr.netbeans.parsing.spi.ParserTaskManager;
 import org.antlr.netbeans.parsing.spi.ParserTaskScheduler;
+import org.netbeans.api.editor.EditorActionRegistration;
 import org.netbeans.api.editor.mimelookup.MimeLookup;
 import org.netbeans.api.editor.mimelookup.MimeRegistration;
 import org.netbeans.api.editor.settings.SimpleValueNames;
 import org.netbeans.editor.BaseAction;
 import org.netbeans.editor.BaseDocument;
+import org.netbeans.editor.BaseKit;
+import org.netbeans.editor.Utilities;
+import org.netbeans.editor.ext.ExtKit;
 import org.netbeans.lib.editor.util.swing.DocumentUtilities;
 import org.netbeans.modules.editor.NbEditorKit;
+import org.openide.awt.Mnemonics;
 import org.openide.util.ContextAwareAction;
 import org.openide.util.ImageUtilities;
 import org.openide.util.Lookup;
@@ -84,7 +94,8 @@ public class GrammarEditorKit extends NbEditorKit {
         ExtendedUncommentAction uncommentAction = new ExtendedUncommentAction(commenter);
 
         Action[] actions = {
-            new ToggleLegacyModeAction()
+            new ToggleLegacyModeAction(),
+            new GrammarGoToDeclarationAction(),
         };
 
         actions = TextAction.augmentList(superActions, actions);
@@ -204,6 +215,126 @@ public class GrammarEditorKit extends NbEditorKit {
                     button.setBorderPainted(legacyMode);
                 }
             }
+        }
+    }
+
+    @NbBundle.Messages({
+        "generate-goto-popup=Go To",
+        "goto-source=Go to Source",
+        "goto_source_open_source_not_formatted=Go to Source",
+    })
+    @EditorActionRegistration(name = generateGoToPopupAction, mimeType = GRAMMAR_MIME_TYPE)
+    public static class GrammarGenerateGoToPopupAction extends NbGenerateGoToPopupAction {
+
+        @Override
+        public void actionPerformed(ActionEvent evt, JTextComponent target) {
+        }
+
+        private void addAcceleretors(Action a, JMenuItem item, JTextComponent target){
+            // Try to get the accelerator
+            Keymap km = target.getKeymap();
+            if (km != null) {
+
+                KeyStroke[] keys = km.getKeyStrokesForAction(a);
+                if (keys != null && keys.length > 0) {
+                    item.setAccelerator(keys[0]);
+                }else if (a!=null){
+                    KeyStroke ks = (KeyStroke)a.getValue(Action.ACCELERATOR_KEY);
+                    if (ks!=null) {
+                        item.setAccelerator(ks);
+                    }
+                }
+            }
+        }
+
+        private void addAction(JTextComponent target, JMenu menu, Action a){
+            if (a != null) {
+                String actionName = (String) a.getValue(Action.NAME);
+                JMenuItem item = null;
+                if (a instanceof BaseAction) {
+                    item = ((BaseAction)a).getPopupMenuItem(target);
+                }
+                if (item == null) {
+                    // gets trimmed text that doesn' contain "go to"
+                    String itemText = (String)a.getValue(ExtKit.TRIMMED_TEXT);
+                    if (itemText == null){
+                        itemText = getItemText(target, actionName, a);
+                    }
+                    if (itemText != null) {
+                        item = new JMenuItem(itemText);
+                        Mnemonics.setLocalizedText(item, itemText);
+                        item.addActionListener(a);
+                        addAcceleretors(a, item, target);
+                        item.setEnabled(a.isEnabled());
+                        Object helpID = a.getValue ("helpID"); // NOI18N
+                        if (helpID != null && (helpID instanceof String))
+                            item.putClientProperty ("HelpID", helpID); // NOI18N
+                    }else{
+                        if (ExtKit.gotoSourceAction.equals(actionName)){
+                            item = new JMenuItem(Bundle.goto_source_open_source_not_formatted());
+                            addAcceleretors(a, item, target);
+                            item.setEnabled(false);
+                        }
+                    }
+                }
+
+                if (item != null) {
+                    menu.add(item);
+                }
+
+            }
+        }
+
+        protected void addAction(JTextComponent target, JMenu menu, String actionName) {
+            BaseKit kit = Utilities.getKit(target);
+            if (kit == null) return;
+            Action a = kit.getActionByName(actionName);
+            if (a!=null){
+                addAction(target, menu, a);
+            } else { // action-name is null, add the separator
+                menu.addSeparator();
+            }
+        }
+
+        protected String getItemText(JTextComponent target, String actionName, Action a) {
+            String itemText;
+            if (a instanceof BaseAction) {
+                itemText = ((BaseAction)a).getPopupMenuText(target);
+            } else {
+                itemText = actionName;
+            }
+            return itemText;
+        }
+
+        @Override
+        public JMenuItem getPopupMenuItem(final JTextComponent target) {
+            String menuText = Bundle.generate_goto_popup();
+            JMenu jm = new JMenu(menuText);
+            //addAction(target, jm, ExtKit.gotoSourceAction);
+            addAction(target, jm, ExtKit.gotoDeclarationAction);
+            //addAction(target, jm, gotoSuperImplementationAction);
+            //addAction(target, jm, ExtKit.gotoAction);
+            return jm;
+        }
+
+    }
+
+    /**
+     * @deprecated Not actually deprecated...
+     */
+    @Deprecated
+    public static class GrammarGoToDeclarationAction extends GotoDeclarationAction {
+
+        public GrammarGoToDeclarationAction() {
+        }
+
+        @Override
+        public boolean gotoDeclaration(JTextComponent target) {
+            if (!(target.getDocument() instanceof StyledDocument))
+                return false;
+
+            GoToSupport.goTo((StyledDocument) target.getDocument(), target.getCaretPosition(), false);
+            return true;
         }
     }
 
