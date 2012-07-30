44da71e75b1263007ab613c11eda9736e96341fc
Sam Harwell <sam@tunnelvisionlabs.com>
2012-07-30 15:39:03 -0500

    Optimize LineTextCache with IntegerList

--- org-antlr-netbeans/src/org/antlr/netbeans/editor/text/impl/LineTextCache.java
+++ org-antlr-netbeans/src/org/antlr/netbeans/editor/text/impl/LineTextCache.java
@@ -9,10 +9,10 @@
 package org.antlr.netbeans.editor.text.impl;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import org.antlr.netbeans.editor.text.DocumentChange;
 import org.antlr.netbeans.editor.text.NormalizedDocumentChangeCollection;
+import org.antlr.v4.runtime.misc.IntegerList;
 import org.netbeans.api.annotations.common.NonNull;
 import org.openide.util.Parameters;
 
@@ -24,21 +24,21 @@ public class LineTextCache {
 
     private static int MaximumBlockLength = 64;
 
-    private final int length;
-    private final int lineCount;
+    private final int _length;
+    private final int _lineCount;
 
-    private final List<List<String>> lineData = new ArrayList<List<String>>();
-    private final List<Integer> blockOffsets = new ArrayList<Integer>();
-    private final List<Integer> blockLineOffsets = new ArrayList<Integer>();
-    private final List<List<Integer>> lineOffsets = new ArrayList<List<Integer>>();
+    private final ArrayList<ArrayList<String>> _lineData = new ArrayList<ArrayList<String>>();
+    private final IntegerList _blockOffsets = new IntegerList();
+    private final IntegerList _blockLineOffsets = new IntegerList();
+    private final ArrayList<IntegerList> _lineOffsets = new ArrayList<IntegerList>();
 
     public LineTextCache(@NonNull String data) {
         Parameters.notNull("data", data);
 
-        length = data.length();
+        _length = data.length();
 
-        List<String> currentBlock = new ArrayList<String>();
-        List<Integer> currentOffsets = new ArrayList<Integer>();
+        ArrayList<String> currentBlock = new ArrayList<String>();
+        IntegerList currentOffsets = new IntegerList();
         @SuppressWarnings("LocalVariableHidesMemberVariable")
         int lineCount = 0;
         int blockStart = 0;
@@ -52,52 +52,54 @@ public class LineTextCache {
                 lineCount++;
                 lineStart = i + 1;
                 if (i == data.length() || currentBlock.size() == MaximumBlockLength) {
-                    lineData.add(currentBlock);
-                    lineOffsets.add(currentOffsets);
-                    blockOffsets.add(blockStart);
-                    blockLineOffsets.add(blockLineStart);
+                    _lineData.add(currentBlock);
+                    _lineOffsets.add(currentOffsets);
+                    _blockOffsets.add(blockStart);
+                    _blockLineOffsets.add(blockLineStart);
                     currentBlock = new ArrayList<String>();
-                    currentOffsets = new ArrayList<Integer>();
+                    currentOffsets = new IntegerList();
                     blockStart = lineStart;
                     blockLineStart = lineCount;
                 }
             }
         }
 
-        this.lineCount = lineCount;
+        this._lineCount = lineCount;
+
+        trimToSize();
     }
 
     private LineTextCache(int length, int lineCount) {
-        this.length = length;
-        this.lineCount = lineCount;
+        this._length = length;
+        this._lineCount = lineCount;
     }
 
     public int getLength() {
-        return length;
+        return _length;
     }
 
     public int getLineCount() {
-        return lineCount;
+        return _lineCount;
     }
 
-    public @NonNull List<List<String>> getLineData() {
-        return lineData;
+    public @NonNull ArrayList<ArrayList<String>> getLineData() {
+        return _lineData;
     }
 
-    public @NonNull List<Integer> getBlockOffsets() {
-        return blockOffsets;
+    public @NonNull IntegerList getBlockOffsets() {
+        return _blockOffsets;
     }
 
-    public @NonNull List<Integer> getBlockLineOffsets() {
-        return blockLineOffsets;
+    public @NonNull IntegerList getBlockLineOffsets() {
+        return _blockLineOffsets;
     }
 
-    public @NonNull List<List<Integer>> getLineOffsets() {
-        return lineOffsets;
+    public @NonNull ArrayList<IntegerList> getLineOffsets() {
+        return _lineOffsets;
     }
 
     public int getBlockFromLineNumber(int lineNumber) {
-        int block = Collections.binarySearch(blockLineOffsets, lineNumber);
+        int block = _blockLineOffsets.binarySearch(lineNumber);
         if (block < 0) {
             block = -(block + 1) - 1;
         }
@@ -106,7 +108,7 @@ public class LineTextCache {
     }
 
     public int getBlockFromPosition(int position) {
-        int block = Collections.binarySearch(blockOffsets, position);
+        int block = _blockOffsets.binarySearch(position);
         if (block < 0) {
             block = -(block + 1) - 1;
         }
@@ -115,7 +117,7 @@ public class LineTextCache {
     }
 
     public int getBlockLineFromPosition(int block, int position) {
-        int blockLine = Collections.binarySearch(lineOffsets.get(block), position - blockOffsets.get(block));
+        int blockLine = _lineOffsets.get(block).binarySearch(position - _blockOffsets.get(block));
         if (blockLine < 0) {
             blockLine = -(blockLine + 1) - 1;
         }
@@ -126,7 +128,7 @@ public class LineTextCache {
     public int getLineNumberFromPosition(int position) {
         int block = getBlockFromPosition(position);
         int blockLine = getBlockLineFromPosition(block, position);
-        return blockLine + blockLineOffsets.get(block);
+        return blockLine + _blockLineOffsets.get(block);
     }
 
     public @NonNull LineTextCache applyChanges(@NonNull NormalizedDocumentChangeCollection changes) {
@@ -137,14 +139,14 @@ public class LineTextCache {
             lineDelta += change.getLineCountDelta();
         }
 
-        LineTextCache next = new LineTextCache(this.length + delta, this.lineCount + lineDelta);
+        LineTextCache next = new LineTextCache(this._length + delta, this._lineCount + lineDelta);
 
         int oldBlock = 0;
         int oldLine = 0;
         int oldColumn = 0;
         int oldBlockLine = 0; // index of oldLine within oldBlock
         int newPosition = 0;
-        List<String> modifiedBlock = null;
+        ArrayList<String> modifiedBlock = null;
         for (int i = 0; i <= changes.size(); i++) {
             DocumentChange currentChange = i < changes.size() ? changes.get(i) : null;
 
@@ -178,7 +180,7 @@ public class LineTextCache {
                 oldLine++;
                 oldColumn = 0;
                 oldBlockLine++;
-                if (oldBlockLine == lineData.get(oldBlock).size()) {
+                if (oldBlockLine == _lineData.get(oldBlock).size()) {
                     oldBlock++;
                     oldBlockLine = 0;
                 }
@@ -188,33 +190,33 @@ public class LineTextCache {
             if (modifiedBlock != null && (currentChange == null || blockEndsBeforeChange(oldBlock, currentChange))) {
                 assert oldColumn == 0 : "Should be at the beginning of a line.";
 
-                if (oldBlock == lineData.size()) {
+                if (oldBlock == _lineData.size()) {
                     // special handling for updates at the end of the last block
                     if (!modifiedBlock.isEmpty()) {
-                        next.lineData.add(modifiedBlock);
+                        next._lineData.add(modifiedBlock);
                     }
 
                     modifiedBlock = null;
                 } else {
-                    List<String> remainingLines = lineData.get(oldBlock).subList(oldBlockLine, lineData.get(oldBlock).size());
-                    if (modifiedBlock.size() + lineData.get(oldBlock).size() - oldBlockLine < MaximumBlockLength) {
+                    List<String> remainingLines = _lineData.get(oldBlock).subList(oldBlockLine, _lineData.get(oldBlock).size());
+                    if (modifiedBlock.size() + _lineData.get(oldBlock).size() - oldBlockLine < MaximumBlockLength) {
                         modifiedBlock.addAll(remainingLines);
                         for (String text : remainingLines) {
                             newPosition += text.length();
                         }
 
                         if (!modifiedBlock.isEmpty()) {
-                            next.lineData.add(modifiedBlock);
+                            next._lineData.add(modifiedBlock);
                         }
                     } else {
                         if (!modifiedBlock.isEmpty()) {
-                            next.lineData.add(modifiedBlock);
+                            next._lineData.add(modifiedBlock);
                         }
 
                         if (oldBlockLine > 0) {
-                            next.lineData.add(new ArrayList<String>(remainingLines));
+                            next._lineData.add(new ArrayList<String>(remainingLines));
                         } else {
-                            next.lineData.add(lineData.get(oldBlock));
+                            next._lineData.add(_lineData.get(oldBlock));
                         }
 
                         for (String text : remainingLines) {
@@ -223,24 +225,24 @@ public class LineTextCache {
                     }
 
                     modifiedBlock = null;
-                    oldLine = blockLineOffsets.get(oldBlock) + lineData.get(oldBlock).size();
+                    oldLine = _blockLineOffsets.get(oldBlock) + _lineData.get(oldBlock).size();
                     oldBlock++;
                     oldBlockLine = 0;
                 }
             }
 
             // 3. move any whole blocks we can
-            while (oldBlock < lineData.size() && (currentChange == null || blockEndsBeforeChange(oldBlock, currentChange))) {
+            while (oldBlock < _lineData.size() && (currentChange == null || blockEndsBeforeChange(oldBlock, currentChange))) {
                 if (modifiedBlock != null) {
                     if (!modifiedBlock.isEmpty()) {
-                        next.lineData.add(modifiedBlock);
+                        next._lineData.add(modifiedBlock);
                     }
                     modifiedBlock = null;
                 }
 
-                newPosition += getBlockEnd(oldBlock) - blockOffsets.get(oldBlock);
-                next.lineData.add(lineData.get(oldBlock));
-                oldLine = blockLineOffsets.get(oldBlock) + lineData.get(oldBlock).size();
+                newPosition += getBlockEnd(oldBlock) - _blockOffsets.get(oldBlock);
+                next._lineData.add(_lineData.get(oldBlock));
+                oldLine = _blockLineOffsets.get(oldBlock) + _lineData.get(oldBlock).size();
                 oldBlock++;
                 oldBlockLine = 0;
             }
@@ -248,7 +250,7 @@ public class LineTextCache {
             // 4. now move any whole lines we can
             while (oldLine < getLineCount() && (currentChange == null || lineEndsBeforeChange(oldBlock, oldLine, currentChange))) {
                 if (modifiedBlock != null && modifiedBlock.size() == MaximumBlockLength) {
-                    next.lineData.add(modifiedBlock);
+                    next._lineData.add(modifiedBlock);
                     modifiedBlock = null;
                 }
 
@@ -256,11 +258,11 @@ public class LineTextCache {
                     modifiedBlock = new ArrayList<String>();
                 }
 
-                modifiedBlock.add(lineData.get(oldBlock).get(oldBlockLine));
+                modifiedBlock.add(_lineData.get(oldBlock).get(oldBlockLine));
                 newPosition += modifiedBlock.get(modifiedBlock.size() - 1).length();
                 oldLine++;
                 oldBlockLine++;
-                assert oldBlockLine < lineData.get(oldBlock).size() : "Should have replaced the rest of the block in stop 2 or 3.";
+                assert oldBlockLine < _lineData.get(oldBlock).size() : "Should have replaced the rest of the block in stop 2 or 3.";
             }
 
             if (currentChange != null) {
@@ -324,14 +326,14 @@ public class LineTextCache {
                 oldBlock = getBlockFromPosition(oldPosition);
                 oldLine = getLineNumberFromPosition(oldPosition);
                 oldColumn = oldPosition - getLineStart(oldBlock, oldLine);
-                oldBlockLine = oldLine - blockLineOffsets.get(oldBlock);
+                oldBlockLine = oldLine - _blockLineOffsets.get(oldBlock);
                 newPosition += newText.length();
             }
         }
 
         if (modifiedBlock != null) {
             if (!modifiedBlock.isEmpty()) {
-                next.lineData.add(modifiedBlock);
+                next._lineData.add(modifiedBlock);
             }
             modifiedBlock = null;
         }
@@ -339,31 +341,33 @@ public class LineTextCache {
         // now set all the indexes in the new cache
         int newBlockOffset = 0; // offset of the start of the current block
         int newLine = 0; // absolute line number
-        for (int newBlock = 0; newBlock < next.lineData.size(); newBlock++) {
-            List<String> block = next.lineData.get(newBlock);
-            next.blockOffsets.add(newBlockOffset);
-            next.blockLineOffsets.add(newLine);
-            next.lineOffsets.add(new ArrayList<Integer>());
+        for (int newBlock = 0; newBlock < next._lineData.size(); newBlock++) {
+            ArrayList<String> block = next._lineData.get(newBlock);
+            next._blockOffsets.add(newBlockOffset);
+            next._blockLineOffsets.add(newLine);
+            next._lineOffsets.add(new IntegerList(MaximumBlockLength));
             int blockLineOffset = 0;
             for (int newBlockLine = 0; newBlockLine < block.size(); newBlockLine++) {
-                next.lineOffsets.get(newBlock).add(blockLineOffset);
+                next._lineOffsets.get(newBlock).add(blockLineOffset);
                 blockLineOffset += block.get(newBlockLine).length();
                 newLine++;
                 // all lines before last end with \n
-                assert (newBlock == next.lineData.size() - 1 && newBlockLine == block.size() - 1) || block.get(newBlockLine).endsWith("\n");
+                assert (newBlock == next._lineData.size() - 1 && newBlockLine == block.size() - 1) || block.get(newBlockLine).endsWith("\n");
                 // last line does not end with \n
-                assert newBlock < next.lineData.size() - 1 || newBlockLine < block.size() - 1 || !block.get(newBlockLine).endsWith("\n");
+                assert newBlock < next._lineData.size() - 1 || newBlockLine < block.size() - 1 || !block.get(newBlockLine).endsWith("\n");
             }
 
-            assert next.lineOffsets.get(newBlock).size() == block.size();
+            assert next._lineOffsets.get(newBlock).size() == block.size();
             newBlockOffset += blockLineOffset;
         }
 
-        assert next.blockOffsets.size() == next.lineData.size();
-        assert next.blockLineOffsets.size() == next.lineData.size();
-        assert next.lineOffsets.size() == next.lineData.size();
-        assert newBlockOffset == next.length;
-        assert newLine == next.lineCount;
+        trimToSize();
+
+        assert next._blockOffsets.size() == next._lineData.size();
+        assert next._blockLineOffsets.size() == next._lineData.size();
+        assert next._lineOffsets.size() == next._lineData.size();
+        assert newBlockOffset == next._length;
+        assert newLine == next._lineCount;
 //        assert next.getLineCount() == next.blockOffsets.get(next.lineData.size() - 1) + next.lineData.get(next.lineData.size() - 1).size() : "Line counts should match";
 //        assert next.getLength() == next.getBlockEnd(next.lineData.size() - 1) : "Lengths should match";
 
@@ -385,39 +389,54 @@ public class LineTextCache {
         }
 
         return getBlockEnd(block) == change.getOldOffset()
-            && block < lineData.size() - 1;
+            && block < _lineData.size() - 1;
     }
 
     public int getBlockEnd(int block) {
-        if (block == lineData.size() - 1) {
+        if (block == _lineData.size() - 1) {
             return getLength();
         }
 
-        return blockOffsets.get(block + 1);
+        return _blockOffsets.get(block + 1);
     }
 
     public int getPosition(int block, int line, int column) {
-        int blockLine = line - blockLineOffsets.get(block);
-        return blockOffsets.get(block) + lineOffsets.get(block).get(blockLine) + column;
+        int blockLine = line - _blockLineOffsets.get(block);
+        return _blockOffsets.get(block) + _lineOffsets.get(block).get(blockLine) + column;
     }
 
     public int getLineStart(int block, int line) {
-        int blockLine = line - blockLineOffsets.get(block);
-        return blockOffsets.get(block) + lineOffsets.get(block).get(blockLine);
+        int blockLine = line - _blockLineOffsets.get(block);
+        return _blockOffsets.get(block) + _lineOffsets.get(block).get(blockLine);
     }
 
     public int getLineEnd(int block, int line) {
-        int blockLine = line - blockLineOffsets.get(block);
-        if (blockLine == lineData.get(block).size() - 1) {
+        int blockLine = line - _blockLineOffsets.get(block);
+        if (blockLine == _lineData.get(block).size() - 1) {
             return getBlockEnd(block);
         }
 
-        return blockOffsets.get(block) + lineOffsets.get(block).get(blockLine + 1);
+        return _blockOffsets.get(block) + _lineOffsets.get(block).get(blockLine + 1);
     }
 
     public @NonNull String getLineText(int block, int line) {
-        int blockLine = line - blockLineOffsets.get(block);
-        return lineData.get(block).get(blockLine);
+        int blockLine = line - _blockLineOffsets.get(block);
+        return _lineData.get(block).get(blockLine);
+    }
+
+    private void trimToSize() {
+        _lineData.trimToSize();
+        _blockOffsets.trimToSize();
+        _blockLineOffsets.trimToSize();
+        _lineOffsets.trimToSize();
+
+        for (ArrayList<?> list : _lineData) {
+            list.trimToSize();
+        }
+
+        for (IntegerList list : _lineOffsets) {
+            list.trimToSize();
+        }
     }
 
 }
