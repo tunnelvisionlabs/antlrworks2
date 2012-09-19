052bd181ebfa8b5059b7a57e61392ba51091d4a1
Sam Harwell <sam@tunnelvisionlabs.com>
2012-09-18 19:33:06 -0500

    Add parse tree navigator for Grammar and StringTemplate files

---
+++ org-antlr-works-editor/src/org/antlr/works/editor/grammar/navigation/GrammarParseTreeNavigatorPanel.java
@@ -0,0 +1,72 @@
+/*
+ *  Copyright (c) 2012 Sam Harwell, Tunnel Vision Laboratories LLC
+ *  All rights reserved.
+ * 
+ *  The source code of this document is proprietary work, and is not licensed for
+ *  distribution. For information about licensing, contact Sam Harwell at:
+ *      sam@tunnelvisionlabs.com
+ */
+package org.antlr.works.editor.grammar.navigation;
+
+import org.antlr.works.editor.antlr4.navigation.TreeNavigatorPanel;
+import org.antlr.works.editor.grammar.GrammarEditorKit;
+import org.netbeans.api.editor.mimelookup.MimeRegistration;
+import org.netbeans.spi.editor.highlighting.HighlightsLayer;
+import org.netbeans.spi.editor.highlighting.HighlightsLayerFactory;
+import org.netbeans.spi.editor.highlighting.ZOrder;
+import org.netbeans.spi.navigator.NavigatorPanel;
+import org.openide.filesystems.FileObject;
+import org.openide.util.Lookup;
+import org.openide.util.NbBundle;
+
+/**
+ *
+ * @author Sam Harwell
+ */
+@NbBundle.Messages({
+    "LBL_ParseTreeDefinition=Parse Tree (Development)"
+})
+@NavigatorPanel.Registration(mimeType=GrammarEditorKit.GRAMMAR_MIME_TYPE, position = 100, displayName = "#LBL_ParseTreeDefinition")
+public class GrammarParseTreeNavigatorPanel extends TreeNavigatorPanel {
+
+    private static volatile GrammarParseTreeNavigatorPanel INSTANCE;
+
+    private FileObject _currentFile;
+
+    public static GrammarParseTreeNavigatorPanel getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    protected FileObject getCurrentFile() {
+        return _currentFile;
+    }
+
+    public void setCurrentFile(FileObject currentFile) {
+        this._currentFile = currentFile;
+    }
+
+        @Override
+    public void panelActivated(Lookup context) {
+        INSTANCE = this;
+        _currentFile = null;
+    }
+
+    @Override
+    public void panelDeactivated() {
+        INSTANCE = null;
+        _currentFile = null;
+    }
+
+    @MimeRegistration(mimeType=GrammarEditorKit.GRAMMAR_MIME_TYPE, service=HighlightsLayerFactory.class)
+    public static final class HighlightsLayerFactoryImpl implements HighlightsLayerFactory {
+
+        @Override
+        public HighlightsLayer[] createLayers(Context context) {
+            return new HighlightsLayer[] {
+                HighlightsLayer.create(GrammarParseTreeNavigatorPanel.class.getName(), ZOrder.DEFAULT_RACK, true, getBag(context.getDocument()))
+            };
+        }
+
+    }
+}
---
+++ org-antlr-works-editor/src/org/antlr/works/editor/grammar/navigation/ParseTreeNavigatorUpdateParserTask.java
@@ -0,0 +1,123 @@
+/*
+ *  Copyright (c) 2012 Sam Harwell, Tunnel Vision Laboratories LLC
+ *  All rights reserved.
+ * 
+ *  The source code of this document is proprietary work, and is not licensed for
+ *  distribution. For information about licensing, contact Sam Harwell at:
+ *      sam@tunnelvisionlabs.com
+ */
+package org.antlr.works.editor.grammar.navigation;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import javax.swing.text.Document;
+import javax.swing.text.JTextComponent;
+import org.antlr.netbeans.editor.text.DocumentSnapshot;
+import org.antlr.netbeans.editor.text.VersionedDocumentUtilities;
+import org.antlr.netbeans.parsing.spi.ParseContext;
+import org.antlr.netbeans.parsing.spi.ParserData;
+import org.antlr.netbeans.parsing.spi.ParserDataDefinition;
+import org.antlr.netbeans.parsing.spi.ParserDataOptions;
+import org.antlr.netbeans.parsing.spi.ParserResultHandler;
+import org.antlr.netbeans.parsing.spi.ParserTask;
+import org.antlr.netbeans.parsing.spi.ParserTaskDefinition;
+import org.antlr.netbeans.parsing.spi.ParserTaskManager;
+import org.antlr.netbeans.parsing.spi.ParserTaskProvider;
+import org.antlr.netbeans.parsing.spi.ParserTaskScheduler;
+import org.antlr.netbeans.parsing.spi.SingletonParserTaskProvider;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.Token;
+import org.antlr.works.editor.antlr4.navigation.ParseTreeNode;
+import org.antlr.works.editor.grammar.GrammarEditorKit;
+import org.antlr.works.editor.grammar.GrammarParserDataDefinitions;
+import org.antlr.works.editor.grammar.experimental.CurrentRuleContextData;
+import org.antlr.works.editor.grammar.parser.CompiledModel;
+import org.netbeans.api.editor.EditorRegistry;
+import org.netbeans.api.editor.mimelookup.MimeRegistration;
+
+/**
+ *
+ * @author Sam Harwell
+ */
+public class ParseTreeNavigatorUpdateParserTask implements ParserTask {
+    private final Object lock = new Object();
+
+    @Override
+    public ParserTaskDefinition getDefinition() {
+        return Definition.INSTANCE;
+    }
+
+    @Override
+    public void parse(ParserTaskManager taskManager, ParseContext parseContext, DocumentSnapshot snapshot, Collection<ParserDataDefinition<?>> requestedData, ParserResultHandler results) throws InterruptedException, ExecutionException {
+        synchronized (lock) {
+            GrammarParseTreeNavigatorPanel panel = GrammarParseTreeNavigatorPanel.getInstance();
+            if (panel == null) {
+                return;
+            }
+
+            JTextComponent currentComponent = EditorRegistry.lastFocusedComponent();
+            if (currentComponent == null) {
+                return;
+            }
+
+            Document document = currentComponent.getDocument();
+            if (document == null || !VersionedDocumentUtilities.getVersionedDocument(document).equals(snapshot.getVersionedDocument())) {
+                return;
+            }
+
+            Future<ParserData<ParserRuleContext<Token>>> futureData = taskManager.getData(snapshot, GrammarParserDataDefinitions.REFERENCE_PARSE_TREE, EnumSet.of(ParserDataOptions.NO_UPDATE));
+            ParserData<ParserRuleContext<Token>> parserData = futureData.get();
+            if (parserData == null) {
+                return;
+            }
+
+            ParserRuleContext<Token> parseTree = parserData.getData();
+
+            Future<ParserData<CurrentRuleContextData>> futureContextData = taskManager.getData(snapshot, GrammarParserDataDefinitions.CURRENT_RULE_CONTEXT, EnumSet.of(ParserDataOptions.NO_UPDATE));
+            ParserData<CurrentRuleContextData> parserContextData = futureContextData.get();
+            CurrentRuleContextData context = null;
+            if (parserContextData != null) {
+                context = parserContextData.getData();
+            }
+
+            panel.setCurrentFile(snapshot.getVersionedDocument().getFileObject());
+            panel.setParseTree(new ParseTreeNode(parseTree));
+        }
+    }
+
+    private static final class Definition extends ParserTaskDefinition {
+        private static final Collection<ParserDataDefinition<?>> INPUTS =
+            Arrays.<ParserDataDefinition<?>>asList(
+                GrammarParserDataDefinitions.COMPILED_MODEL,
+                GrammarParserDataDefinitions.REFERENCE_PARSE_TREE);
+
+        private static final Collection<ParserDataDefinition<?>> OUTPUTS =
+            Collections.<ParserDataDefinition<?>>emptyList();
+
+        public static final Definition INSTANCE = new Definition();
+
+        public Definition() {
+            super("Grammar Parse Tree Navigator Update", INPUTS, OUTPUTS, ParserTaskScheduler.INPUT_SENSITIVE_TASK_SCHEDULER);
+        }
+    }
+
+    @MimeRegistration(mimeType=GrammarEditorKit.GRAMMAR_MIME_TYPE, service=ParserTaskProvider.class)
+    public static final class Provider extends SingletonParserTaskProvider {
+
+        @Override
+        public ParserTaskDefinition getDefinition() {
+            return Definition.INSTANCE;
+        }
+
+        @Override
+        public ParserTask createTaskImpl() {
+            return new ParseTreeNavigatorUpdateParserTask();
+        }
+
+    }
+
+}
