ef87b3dbfe4b0c4523da57bfaec46700671d478e
Sam Harwell <sam@tunnelvisionlabs.com>
2013-01-12 14:49:24 -0600

    Code generator prints output to ANTLR Codegen output window, runs in its own RequestProcessor

--- org-antlr-works-editor/src/org/antlr/works/editor/grammar/codegen/CodeGenerator.java
+++ org-antlr-works-editor/src/org/antlr/works/editor/grammar/codegen/CodeGenerator.java
@@ -12,21 +12,27 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import org.antlr.v4.Tool;
+import org.netbeans.api.annotations.common.NonNull;
 import org.netbeans.api.annotations.common.StaticResource;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.openide.util.Exceptions;
+import org.openide.util.RequestProcessor;
+import org.openide.util.Task;
 import org.openide.util.Utilities;
+import org.openide.windows.IOProvider;
+import org.openide.windows.InputOutput;
+import org.openide.windows.OutputWriter;
 
 /**
  *
@@ -35,6 +41,11 @@ import org.openide.util.Utilities;
 public class CodeGenerator {
     @StaticResource
     private static final String ANTLR4_COMPLETE_JAR = "org/antlr/works/editor/grammar/resources/antlr4-complete.jar";
+
+    private static File referenceLibrary;
+    private static ClassLoader referenceClassLoader;
+
+    public static final RequestProcessor REFERENCE_RP = new RequestProcessor("ANTLR Reference Tool");
 
     public final String target;
     public final FileObject[] grammarFiles;
@@ -59,52 +70,112 @@ public class CodeGenerator {
         this.grammarFiles = grammarFiles;
     }
 
-    public void run() {
-        try {
-            ClassLoader loader;
-            if (target.contains("sharwell/optimized")) {
-                loader = Thread.currentThread().getContextClassLoader();
-            } else {
-                File completeJar = copyCompleteJarToTempDir();
-                loader = new URLClassLoader(new URL[] { Utilities.toURI(completeJar).toURL() }, ClassLoader.getSystemClassLoader());
-            }
-
-            Class<?> toolClass = loader.loadClass(Tool.class.getName());
-            Constructor<?> ctor = toolClass.getConstructor(String[].class);
-            Method processGrammarsOnCommandLine = toolClass.getMethod("processGrammarsOnCommandLine");
-
-            List<String> args = getCommandArguments();
-            for (FileObject grammarFile : grammarFiles) {
-                args.add(FileUtil.toFile(grammarFile).getAbsolutePath());
-            }
-
-            ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
-            Thread.currentThread().setContextClassLoader(loader);
-            try {
-                Object tool = ctor.newInstance((Object)args.toArray(new String[args.size()]));
-                processGrammarsOnCommandLine.invoke(tool);
-            } finally {
-                Thread.currentThread().setContextClassLoader(contextClassLoader);
-            }
-        } catch (ClassNotFoundException ex) {
-            Exceptions.printStackTrace(ex);
-        } catch (NoSuchMethodException ex) {
-            Exceptions.printStackTrace(ex);
-        } catch (SecurityException ex) {
-            Exceptions.printStackTrace(ex);
-        } catch (InstantiationException ex) {
-            Exceptions.printStackTrace(ex);
-        } catch (IllegalAccessException ex) {
-            Exceptions.printStackTrace(ex);
-        } catch (IllegalArgumentException ex) {
-            Exceptions.printStackTrace(ex);
-        } catch (InvocationTargetException ex) {
-            Exceptions.printStackTrace(ex);
-        } catch (MalformedURLException ex) {
-            Exceptions.printStackTrace(ex);
-        } catch (IOException ex) {
-            Exceptions.printStackTrace(ex);
+    public static ClassLoader getReferenceClassLoader() {
+        if (referenceClassLoader != null) {
+            return referenceClassLoader;
         }
+
+        REFERENCE_RP.post(new Runnable() {
+            @Override
+            public void run() {
+                if (referenceClassLoader != null) {
+                    return;
+                }
+
+                try {
+                    referenceLibrary = copyCompleteJarToTempDir();
+                    referenceClassLoader = new URLClassLoader(new URL[]{Utilities.toURI(referenceLibrary).toURL()}, ClassLoader.getSystemClassLoader());
+                } catch (IOException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+            }
+        }).waitFinished();
+
+        return referenceClassLoader;
+    }
+
+    public static File getReferenceLibrary() {
+        getReferenceClassLoader();
+        return referenceLibrary;
+    }
+
+    public Task run() {
+        ClassLoader targetLoader;
+        if (target.contains("sharwell/optimized")) {
+            targetLoader = Thread.currentThread().getContextClassLoader();
+        } else {
+            targetLoader = getReferenceClassLoader();
+        }
+
+        if (targetLoader == null) {
+            return RequestProcessor.Task.EMPTY;
+        }
+
+        final ClassLoader loader = targetLoader;
+
+        return REFERENCE_RP.post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Class<?> toolClass = loader.loadClass(Tool.class.getName());
+                    Constructor<?> ctor = toolClass.getConstructor(String[].class);
+                    Method processGrammarsOnCommandLine = toolClass.getMethod("processGrammarsOnCommandLine");
+
+                    List<String> args = getCommandArguments();
+                    for (FileObject grammarFile : grammarFiles) {
+                        args.add(FileUtil.toFile(grammarFile).getAbsolutePath());
+                    }
+
+                    ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+                    Thread.currentThread().setContextClassLoader(loader);
+                    try {
+                        InputOutput inputOutput = IOProvider.getDefault().getIO(String.format("ANTLR Codegen (%s)", target), false);
+                        inputOutput.select();
+                        PrintStream originalOut = System.out;
+                        OutputWriter outputWriter = inputOutput.getOut();
+                        try {
+                            System.setOut(new PrintStream(new OutputWriterStream(outputWriter)));
+                            try {
+                                PrintStream originalErr = System.err;
+                                OutputWriter errorWriter = inputOutput.getErr();
+                                try {
+                                    System.setErr(new PrintStream(new OutputWriterStream(errorWriter)));
+                                    try {
+                                        outputWriter.format("Arguments: %s%n", args);
+                                        Object tool = ctor.newInstance((Object)args.toArray(new String[args.size()]));
+                                        processGrammarsOnCommandLine.invoke(tool);
+                                    } finally {
+                                        System.setErr(originalErr);
+                                    }
+                                } finally {
+                                    errorWriter.close();
+                                }
+                            } finally {
+                                System.setOut(originalOut);
+                            }
+                        } finally {
+                            outputWriter.close();
+                        }
+                    } finally {
+                        Thread.currentThread().setContextClassLoader(contextClassLoader);
+                    }
+                } catch (ClassNotFoundException ex) {
+                    Exceptions.printStackTrace(ex);
+                } catch (NoSuchMethodException ex) {
+                    Exceptions.printStackTrace(ex);
+                } catch (SecurityException ex) {
+                    Exceptions.printStackTrace(ex);
+                } catch (InstantiationException ex) {
+                    Exceptions.printStackTrace(ex);
+                } catch (IllegalAccessException ex) {
+                    Exceptions.printStackTrace(ex);
+                } catch (IllegalArgumentException ex) {
+                    Exceptions.printStackTrace(ex);
+                } catch (InvocationTargetException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+            }
+        });
     }
 
     private static File copyCompleteJarToTempDir() throws IOException {
@@ -193,4 +264,36 @@ public class CodeGenerator {
         return args;
     }
 
+    public static class OutputWriterStream extends OutputStream {
+        private final OutputWriter writer;
+
+        public OutputWriterStream(@NonNull OutputWriter writer) {
+            this.writer = writer;
+        }
+
+        @Override
+        public void close() throws IOException {
+            writer.close();
+        }
+
+        @Override
+        public void flush() throws IOException {
+            writer.flush();
+        }
+
+        @Override
+        public void write(byte[] b) throws IOException {
+            writer.write(new String(b));
+        }
+
+        @Override
+        public void write(byte[] b, int off, int len) throws IOException {
+            writer.write(new String(b, off, len));
+        }
+
+        @Override
+        public void write(int b) throws IOException {
+            writer.write(b);
+        }
+    }
 }
