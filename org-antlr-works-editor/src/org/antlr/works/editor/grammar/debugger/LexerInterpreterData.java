8f432059a67d575fa77c90beb35472e4322ddab4
Sam Harwell <sam@tunnelvisionlabs.com>
2014-01-28 06:35:01 -0600

    Updated LexerInterpreterData.buildFromSnapshot to work with parser grammars via the tokenVocab option

--- org-antlr-works-editor/src/org/antlr/works/editor/grammar/debugger/LexerInterpreterData.java
+++ org-antlr-works-editor/src/org/antlr/works/editor/grammar/debugger/LexerInterpreterData.java
@@ -11,19 +11,26 @@ package org.antlr.works.editor.grammar.debugger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.antlr.netbeans.editor.text.DocumentSnapshot;
+import org.antlr.netbeans.editor.text.VersionedDocument;
+import org.antlr.netbeans.editor.text.VersionedDocumentUtilities;
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.v4.Tool;
+import org.antlr.v4.parse.ANTLRParser;
 import org.antlr.v4.runtime.atn.ATNSerializer;
 import org.antlr.v4.tool.Grammar;
 import org.antlr.v4.tool.LexerGrammar;
 import org.antlr.v4.tool.ast.GrammarRootAST;
+import org.openide.filesystems.FileObject;
 
 /**
  *
  * @author Sam Harwell
  */
 public class LexerInterpreterData extends AbstractInterpreterData {
+    private static final Logger LOGGER = Logger.getLogger(LexerInterpreterData.class.getName());
 
     public List<String> modeNames;
 
@@ -33,6 +40,35 @@ public class LexerInterpreterData extends AbstractInterpreterData {
         ANTLRStringStream stream = new ANTLRStringStream(snapshot.getText());
         stream.name = snapshot.getVersionedDocument().getFileObject().getNameExt();
         GrammarRootAST ast = tool.parse(stream.name, stream);
+        if (ast.grammarType == ANTLRParser.PARSER) {
+            // load the correct lexer
+            String vocabName = ast.getOptionString("tokenVocab");
+
+            // try to find a lexer in the same folder with this name
+            FileObject containingFolder = snapshot.getVersionedDocument().getFileObject().getParent();
+            if (containingFolder == null) {
+                LOGGER.log(Level.WARNING, "Could not find source for token vocabulary.");
+                return null;
+            }
+
+            FileObject sourceFileObject = containingFolder.getFileObject(vocabName, "g4");
+            if (sourceFileObject == null) {
+                sourceFileObject = containingFolder.getFileObject(vocabName, "g3");
+            }
+
+            if (sourceFileObject == null) {
+                sourceFileObject = containingFolder.getFileObject(vocabName, "g");
+            }
+
+            if (sourceFileObject == null) {
+                LOGGER.log(Level.WARNING, "Could not find source for token vocabulary.");
+                return null;
+            }
+
+            VersionedDocument sourceDocument = VersionedDocumentUtilities.getVersionedDocument(sourceFileObject);
+            return buildFromSnapshot(sourceDocument.getCurrentSnapshot());
+        }
+
         Grammar grammar = tool.createGrammar(ast);
         tool.process(grammar, false);
 
